9acb64208a7869661d75902622985cfebc2d4d04
NorthRealm <155140859+NorthRealm@users.noreply.github.com>
2025-10-23 18:33:12 +0800

    Update

--- src/test/java/colosseum/utility/unittest/TestTeamNames.java
+++ src/test/java/colosseum/utility/unittest/TestTeamNames.java
@@ -1,12 +1,13 @@
 package colosseum.utility.unittest;
 
 import colosseum.utility.TeamNames;
+import org.bukkit.DyeColor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestTeamNames {
     @Test
-    void test() {
+    void testIndex() {
         TeamNames[] teamNames = TeamNames.values();
         Assertions.assertEquals("Orange", teamNames[1].name());
         Assertions.assertEquals("Magenta", teamNames[2].name());
@@ -23,5 +24,35 @@ class TestTeamNames {
         Assertions.assertEquals("Green", teamNames[13].name());
         Assertions.assertEquals("Red", teamNames[14].name());
         Assertions.assertEquals("Black", teamNames[15].name());
+    }
+
+    @Test
+    void testData() {
+        TeamNames[] teamNames = TeamNames.values();
+        DyeColor[] colors = DyeColor.values();
+        Assertions.assertEquals(colors.length, teamNames.length);
+        for (int i = 0; i < teamNames.length; i++) {
+            Assertions.assertEquals(colors[i].getWoolData(), teamNames[i].getWoolData());
+        }
+    }
+
+    @Test
+    void testGetByDyeColor() {
+        Assertions.assertEquals(TeamNames.White, TeamNames.getByDyeColor(DyeColor.WHITE.name()));
+        Assertions.assertEquals(TeamNames.Orange, TeamNames.getByDyeColor(DyeColor.ORANGE.name()));
+        Assertions.assertEquals(TeamNames.Magenta, TeamNames.getByDyeColor(DyeColor.MAGENTA.name()));
+        Assertions.assertEquals(TeamNames.Sky, TeamNames.getByDyeColor(DyeColor.LIGHT_BLUE.name()));
+        Assertions.assertEquals(TeamNames.Yellow, TeamNames.getByDyeColor(DyeColor.YELLOW.name()));
+        Assertions.assertEquals(TeamNames.Lime, TeamNames.getByDyeColor(DyeColor.LIME.name()));
+        Assertions.assertEquals(TeamNames.Pink, TeamNames.getByDyeColor(DyeColor.PINK.name()));
+        Assertions.assertEquals(TeamNames.Gray, TeamNames.getByDyeColor(DyeColor.GRAY.name()));
+        Assertions.assertEquals(TeamNames.LGray, TeamNames.getByDyeColor(DyeColor.SILVER.name()));
+        Assertions.assertEquals(TeamNames.Cyan, TeamNames.getByDyeColor(DyeColor.CYAN.name()));
+        Assertions.assertEquals(TeamNames.Purple, TeamNames.getByDyeColor(DyeColor.PURPLE.name()));
+        Assertions.assertEquals(TeamNames.Blue, TeamNames.getByDyeColor(DyeColor.BLUE.name()));
+        Assertions.assertEquals(TeamNames.Brown, TeamNames.getByDyeColor(DyeColor.BROWN.name()));
+        Assertions.assertEquals(TeamNames.Green, TeamNames.getByDyeColor(DyeColor.GREEN.name()));
+        Assertions.assertEquals(TeamNames.Red, TeamNames.getByDyeColor(DyeColor.RED.name()));
+        Assertions.assertEquals(TeamNames.Black, TeamNames.getByDyeColor(DyeColor.BLACK.name()));
     }
 }
